a4c0975c8aabcbbb5f4f8bfa8260c9274c272c85
AakashBelide <aakash.belide@gmail.com>
2021-06-24 20:21:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './App.css';
+import Sidebar from './Sidebar.js';
+import Chat from './Chat.js';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app__body">
+        <Sidebar/>
+        <Chat/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB4x7PzsWjXY1ymUUX1xDWNcYcl_MrCFoo",
+    authDomain: "whatsapp-web-clone-240c6.firebaseapp.com",
+    projectId: "whatsapp-web-clone-240c6",
+    storageBucket: "whatsapp-web-clone-240c6.appspot.com",
+    messagingSenderId: "81507467899",
+    appId: "1:81507467899:web:64c72b15e48097523f98de"
+  };
