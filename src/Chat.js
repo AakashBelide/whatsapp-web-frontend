ff4882e22dabe247003d49177ae3cce2360bad71
AakashBelide <aakash.belide@gmail.com>
2021-06-25 22:22:29 +0530

    Frontend

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,62 @@
 import React from 'react'
 import './Chat.css';
+import { Avatar, IconButton } from '@material-ui/core';
+import { AttachFile, SearchOutlined, MoreVert, InsertEmoticon, MicOutlined} from '@material-ui/icons';
 
 function Chat() {
     return (
         <div className="chat">
-            <h1>I am the chat component.</h1>
+            <div className="chat__header">
+                <Avatar/>
+                <div className="chat__headerInfo">
+                    <h3>Room Name</h3>
+                    <p>Last seen at...</p>
+                </div>
+
+                <div className="chat__headerRight">
+                    <IconButton>
+                        <SearchOutlined/>
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile/>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert/>
+                    </IconButton>
+                </div>
+            </div>
+            <div className="chat__body">
+                <p className="chat__message">
+                    <span className="chat__name">Aakash</span>
+                    <br></br>
+                    This is the message
+                    <span className="chat__timestamp">{
+                    new Date().toLocaleTimeString()}</span>
+                </p>
+                <p className="chat__message chat__receiver">
+                    <span className="chat__name">Aakash</span>
+                    <br></br>
+                    This is the message
+                    <span className="chat__timestamp">{
+                    new Date().toLocaleTimeString()}</span>
+                </p>
+                <p className="chat__message">
+                    <span className="chat__name">Aakash</span>
+                    <br></br>
+                    This is the message
+                    <span className="chat__timestamp">{
+                    new Date().toLocaleTimeString()}</span>
+                </p>
+            </div>
+
+            <div className="chat__footer">
+                <InsertEmoticon/>
+                <form>
+                    <input placeholder="Type a message" type="text"/>
+                    <button type="submit"> Send a message</button>
+                </form>
+                <MicOutlined/>
+            </div>
         </div>
     )
 }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,40 @@
 import React from 'react'
 import './Sidebar.css';
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { Avatar, IconButton} from "@material-ui/core";
+import { SearchOutlined } from '@material-ui/icons';
+import SidebarChat from './SidebarChat.js';
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <h1>I am the sidebar.</h1>
+            <div className="sidebar__header">
+                <Avatar src="https://avatars.githubusercontent.com/u/41340373?v=4"/>
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLargeIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon/>
+                    </IconButton>
+                </div>
+            </div>
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined/>
+                    <input placeholder="Search or start new chat" type="text" />
+                </div>
+            </div>
+            <div className="sidebar__chats">
+                <SidebarChat/>
+                <SidebarChat/>
+                <SidebarChat/>
+            </div>
         </div>
     )
 }
